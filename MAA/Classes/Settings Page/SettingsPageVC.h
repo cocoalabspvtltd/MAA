bfe3f5d19e3b05902b25f98f911ff45474c2dfdc
Vineeth Vijayan <vinbhai4u@gmail.com>
2016-01-28 11:46:12 +0530

    Rearranged Tab Bar, Fixed design bugs Commit

---
+++ MAA/Classes/Settings Page/SettingsPageVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsPageVC.h
+//  MAA
+//
+//  Created by Vineeth Vijayan on 28/01/16.
+//  Copyright © 2016 Cocoa Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsPageVC : UIViewController
+
+@end
---
+++ MAA/Classes/Settings Page/SettingsPageVC.m
@@ -0,0 +1,42 @@
+//
+//  SettingsPageVC.m
+//  MAA
+//
+//  Created by Vineeth Vijayan on 28/01/16.
+//  Copyright © 2016 Cocoa Labs. All rights reserved.
+//
+
+#import "SettingsPageVC.h"
+
+@interface SettingsPageVC ()
+
+@end
+
+@implementation SettingsPageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+        
+}
+
+//- (void)viewWillAppear:(BOOL)animated{
+//    self.tabBarController.navigationItem.hidesBackButton = YES;
+//}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
