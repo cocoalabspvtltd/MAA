d4a988ec7e52236461ad2393f2557c94006cdb84
Bibin Cocoalabs <bibin@cocoalabs.in>
2016-02-02 23:23:40 +0530

    AskQuestions page created.

---
+++ AskedQuestions.h
@@ -0,0 +1,14 @@
+//
+//  AskedQuestions.h
+//  MAA
+//
+//  Created by Cocoalabs India on 02/02/16.
+//  Copyright © 2016 Cocoa Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AskedQuestions : UITableViewController
+
+
+@end
---
+++ AskedQuestionsViewController.h
@@ -0,0 +1,18 @@
+//
+//  AskedQuestionsViewController.h
+//  MAA
+//
+//  Created by Cocoalabs India on 02/02/16.
+//  Copyright © 2016 Cocoa Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AskedQuestionsViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UIImageView *floatimage;
+@property (weak, nonatomic) IBOutlet UIButton *floatbutton;
+@property (weak, nonatomic) IBOutlet UITableView *tblquestions;
+@property (weak, nonatomic) IBOutlet UIImageView *ImgProfile;
+@property (strong, nonatomic) IBOutlet UITableViewCell *celll;
+@property (weak, nonatomic) IBOutlet UIView *childview;
+@end
