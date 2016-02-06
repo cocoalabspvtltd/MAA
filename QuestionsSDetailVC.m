08f02687b66c4d34d0945c564bbb51cf6cfad33d
Cocoalabs India <info@cocoalabs.in>
2016-02-06 12:26:46 +0530

    Questions detail page implemented

--- QuestionsSDetailVC.m
+++ QuestionsSDetailVC.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self callingGetQuestionsDetailsApi];
     // Do any additional setup after loading the view.
 }
 
@@ -23,6 +24,72 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(void)callingGetQuestionsDetailsApi{
+    NSString *accessToken = [[NSUserDefaults standardUserDefaults] valueForKey:ACCESS_TOKEN];
+    NSMutableDictionary *getQuestionsMutableDictionary = [[NSMutableDictionary alloc] init];
+    //[getQuestionsMutableDictionary setValue:getQuestionsText forKey:@"keyword"];
+    [getQuestionsMutableDictionary setValue:accessToken forKey:@"token"];
+    [getQuestionsMutableDictionary setValue:self.questionId forKey:@"id"];
+    //[getQuestionsMutableDictionary setValue:[NSNumber numberWithInt:self.offsetValue] forKey:Offsetkey];
+   // [getQuestionsMutableDictionary setValue:[NSNumber numberWithInt:self.limitValue] forKey:LimitKey];
+    NSString *getQuestionsUrlString = [Baseurl stringByAppendingString:GetQuestionsApiUrl];
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [[NetworkHandler sharedHandler] requestWithRequestUrl:[NSURL URLWithString:getQuestionsUrlString] withBody:getQuestionsMutableDictionary withMethodType:HTTPMethodPOST withAccessToken:accessToken];
+    [[NetworkHandler sharedHandler] startServieRequestWithSucessBlockSuccessBlock:^(id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        [self populatingDetailPageOfQuestiosWithData:[[responseObject valueForKey:Datakey] objectAtIndex:0]];
+        NSLog(@"Response Object:%@",responseObject);
+    } FailureBlock:^(NSString *errorDescription, id errorResponse) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        NSString *errorMessage;
+        if([errorDescription isEqualToString:NoNetworkErrorName]){
+            errorMessage = NoNetworkmessage;
+        }
+        else{
+            errorMessage = ConnectiontoServerFailedMessage;
+        }
+        UIAlertView *erroralert = [[UIAlertView alloc] initWithTitle:AppName message:errorMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [erroralert show];
+    }];
+}
+
+
+-(void)populatingDetailPageOfQuestiosWithData:(id)detailsData{
+    self.questionLabel.text = [detailsData valueForKey:@"title"];
+    self.questionDetailLabe.text = [detailsData valueForKey:@"description"];
+    self.dateLabel.text = [detailsData valueForKey:@"question_timestamp"];
+    self.doctorNameLabel.text = [detailsData valueForKey:@"answeree_name"];
+    self.doctorSpecialityLabel.text = [detailsData valueForKey:@"tagline"];
+    self.answerDateLabel.text = [detailsData valueForKey:@"answer_timestamp"];
+    self.answerLabe.text = [detailsData valueForKey:@"answer"];
+    [self settingDoctorProfileImageWithUrlStrimg:[detailsData valueForKey:@"answeree_image"] withAnswereID: [detailsData valueForKey:@"answeree_id"]];
+}
+
+-(void)settingDoctorProfileImageWithUrlStrimg:(NSString *)profileImageUrlString withAnswereID:(NSString *)answereID{
+        NSString *folderPath = [NSString stringWithFormat:@"Maa/Photos/Doctor"];
+        NSURL *imageUrl = [NSURL URLWithString:profileImageUrlString];
+        UIImage *localImage;
+        localImage = [[ImageCache sharedCache] imageFromFolder:folderPath WithIdentifier:answereID];
+        if(!localImage){
+            [MBProgressHUD showHUDAddedTo:self.doctorProfileImageView animated:YES];
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                NSData *imageData = [NSData dataWithContentsOfURL:imageUrl];
+                UIImage *tempImage = [UIImage imageWithData:imageData];
+                [[ImageCache sharedCache]addImage:tempImage toFolder:folderPath toCacheWithIdentifier:answereID];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.doctorProfileImageView.image = tempImage;
+                    [MBProgressHUD hideAllHUDsForView:self.doctorProfileImageView animated:YES];
+                }
+                               );
+            });
+        }
+        else{
+            self.doctorProfileImageView.image = localImage;
+        }
+        
+    }
 
 /*
 #pragma mark - Navigation
