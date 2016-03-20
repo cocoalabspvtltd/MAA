b4585dd806579cff376b226a857c590d1de7a85e
Cocoalabs India <info@cocoalabs.in>
2016-03-20 13:48:42 +0530

    Docto Profile views are rearranged.

--- MAA/ViewControllers/DetailPage/DoctorProfilePage/DoctorProfVC.h
+++ MAA/ViewControllers/DetailPage/DoctorProfilePage/DoctorProfVC.h
@@ -10,6 +10,15 @@
 
 @interface DoctorProfVC : UIViewController
 @property (weak, nonatomic) IBOutlet UIImageView *imgProfile;
+@property (strong, nonatomic) IBOutlet UILabel *namLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
+@property (strong, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) IBOutlet UILabel *experienceLbael;
 @property (weak, nonatomic) IBOutlet UIButton *btnDirection;
+@property (strong, nonatomic) IBOutlet UILabel *consultationFeeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *noOfConsultationsLabel;
+@property (strong, nonatomic) IBOutlet UILabel *reviewLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (strong, nonatomic) IBOutlet UILabel *contactNoLabel;
 @property (nonatomic, strong) NSString *entityId;
 @end
