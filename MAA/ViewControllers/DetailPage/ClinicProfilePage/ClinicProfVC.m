2c9fd60314ce003d6b7f30eb64d6703cc409de21
Bibin Cocoalabs <bibin@cocoalabs.in>
2016-03-22 22:48:31 +0530

    GetClinic Details and Get DoctorDetailsApi implemented.

--- MAA/ViewControllers/DetailPage/ClinicProfilePage/ClinicProfVC.m
+++ MAA/ViewControllers/DetailPage/ClinicProfilePage/ClinicProfVC.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self callingGetClinicDetailsApi];
     // Do any additional setup after loading the view.
 }
 
@@ -36,5 +37,35 @@
 - (IBAction)backButtonAction:(UIButton *)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+-(void)callingGetClinicDetailsApi{
+    NSString *getEntityDetailsUrlString = [Baseurl stringByAppendingString:GetEntityDetailsUrl];
+    NSString *accesstoken = [[NSUserDefaults standardUserDefaults] valueForKey:ACCESS_TOKEN];
+    NSMutableDictionary *getEntityDetailsMutableDictionary = [[NSMutableDictionary alloc] init];
+    [getEntityDetailsMutableDictionary setValue:accesstoken forKey:@"token"];
+    [getEntityDetailsMutableDictionary setValue:self.entityId forKey:@"id"];
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [[NetworkHandler sharedHandler] requestWithRequestUrl:[NSURL URLWithString:getEntityDetailsUrlString] withBody:getEntityDetailsMutableDictionary withMethodType:HTTPMethodPOST withAccessToken:accesstoken];
+    [[NetworkHandler sharedHandler] startServieRequestWithSucessBlockSuccessBlock:^(id responseObject) {
+        NSLog(@"Hospital Detilst;%@",responseObject);
+        //self.hospitalDataDictionary = [responseObject valueForKey:Datakey];
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        //[self settingEntityDetailsWithData:[responseObject valueForKey:Datakey]];
+        //[self.clinicTbableView reloadData];
+    } FailureBlock:^(NSString *errorDescription, id errorResponse) {
+        NSLog(@"Respnse Error;%@",errorResponse);
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        NSString *errorMessage;
+        if([errorDescription isEqualToString:NoNetworkErrorName]){
+            errorMessage = NoNetworkmessage;
+        }
+        else{
+            errorMessage = ConnectiontoServerFailedMessage;
+        }
+        UIAlertView *erroralert = [[UIAlertView alloc] initWithTitle:AppName message:errorMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [erroralert show];
+    }];
+}
+
 
 @end
--- MAA/ViewControllers/DetailPage/DoctorProfilePage/DoctorProfVC.m
+++ MAA/ViewControllers/DetailPage/DoctorProfilePage/DoctorProfVC.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self callingGetDoctorDetailsApi];
     // Do any additional setup after loading the view.
 }
 
@@ -36,5 +37,42 @@
 - (IBAction)backButtonAction:(UIButton *)sender {
     [self.navigationController popViewControllerAnimated:YES];
 }
+
+#pragma mark - Get Entity Details Api
+
+-(void)callingGetDoctorDetailsApi{
+    NSLog(@"Get Doctor Id;%@",self.entityId);
+    NSString *getEntityDetailsUrlString = [Baseurl stringByAppendingString:GetEntityDetailsUrl];
+    NSString *accesstoken = [[NSUserDefaults standardUserDefaults] valueForKey:ACCESS_TOKEN];
+    NSMutableDictionary *getEntityDetailsMutableDictionary = [[NSMutableDictionary alloc] init];
+    [getEntityDetailsMutableDictionary setValue:accesstoken forKey:@"token"];
+    [getEntityDetailsMutableDictionary setValue:self.entityId forKey:@"id"];
+    [getEntityDetailsMutableDictionary setValue:[NSNumber numberWithInt:1] forKey:@"format"];
+    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [[NetworkHandler sharedHandler] requestWithRequestUrl:[NSURL URLWithString:getEntityDetailsUrlString] withBody:getEntityDetailsMutableDictionary withMethodType:HTTPMethodPOST withAccessToken:accesstoken];
+    [[NetworkHandler sharedHandler] startServieRequestWithSucessBlockSuccessBlock:^(id responseObject) {
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        NSLog(@"Response :%@",responseObject);
+        //[self settingEntityDetailsWithData:[responseObject valueForKey:Datakey]];
+       // self.clinicDetailsArray = [[responseObject valueForKey:Datakey] valueForKey:@"clinic_details"];
+       // self.servicesArray = [[responseObject valueForKey:Datakey] valueForKey:@"attributes"];
+        //NSLog(@"Services Array:%@",self.servicesArray);
+       // [self.doctoDetailsTableView reloadData];
+        
+    } FailureBlock:^(NSString *errorDescription, id errorResponse) {
+        NSLog(@"Respnse Error;%@",errorResponse);
+        [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
+        NSString *errorMessage;
+        if([errorDescription isEqualToString:NoNetworkErrorName]){
+            errorMessage = NoNetworkmessage;
+        }
+        else{
+            errorMessage = ConnectiontoServerFailedMessage;
+        }
+        UIAlertView *erroralert = [[UIAlertView alloc] initWithTitle:AppName message:errorMessage delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+        [erroralert show];
+    }];
+}
+
 
 @end
