ae8ac99eeecfe2af5bfb1c816d82b6f86ab4a695
Cocoalabs India <info@cocoalabs.in>
2016-04-03 22:56:39 +0530

    Age,Fee and Experience pickers are populated.

--- MAA/ViewControllers/HomeTab/Search Filter/SearchFilterVC.m
+++ MAA/ViewControllers/HomeTab/Search Filter/SearchFilterVC.m
@@ -8,6 +8,9 @@
 
 #define TypePickerViewTag 10
 #define GenderPickerViewTag 20
+#define AgePickerViewTag 30
+#define FeePickerViewTag 40
+#define ExperiencePickerViewTag 50
 
 #import "SearchFilterVC.h"
 
@@ -15,16 +18,28 @@
 {
     UIPickerView *typePickerview;
     UIPickerView *genderPickerView;
+    UIPickerView *agePickerView;
+    UIPickerView *feePickerView;
+    UIPickerView *experiencePickerView;
     UITapGestureRecognizer *gesture;
     
     
 }
 @property (nonatomic, strong) id selectedType;
 @property (nonatomic, strong) id selectedGender;
+@property (nonatomic, strong) id selectedFromAge;
+@property (nonatomic, strong) id selectedToAge;
+@property (nonatomic, strong) id selectedFromFee;
+@property (nonatomic, strong) id selectedTofee;
+@property (nonatomic, strong) id selectedFromExperience;
+@property (nonatomic, strong) id selectedToExperience;
 
 @property (nonatomic, strong) id filterCriteriaData;
 @property (nonatomic, strong) NSArray *typeArray;
 @property (nonatomic, strong) NSArray *genderArray;
+@property (nonatomic, strong) NSArray *ageArray;
+@property (nonatomic, strong) NSArray *feeArray;
+@property (nonatomic, strong) NSArray *experienceArray;
 @end
 
 @implementation SearchFilterVC
@@ -44,6 +59,28 @@
     genderPickerView.delegate = self;
     genderPickerView.dataSource = self;
     genderPickerView.tag = GenderPickerViewTag;
+    
+    
+    agePickerView = [[UIPickerView alloc]init];
+    _txtAgeFrom.inputView = agePickerView;
+    _txtAgeTo.inputView = agePickerView;
+    agePickerView.delegate = self;
+    agePickerView.dataSource = self;
+    agePickerView.tag = AgePickerViewTag;
+    
+    feePickerView = [[UIPickerView alloc]init];
+    _txtFeeFrom.inputView = feePickerView;
+    _txtFeeTo.inputView = feePickerView;
+    feePickerView.delegate = self;
+    feePickerView.dataSource = self;
+    feePickerView.tag = FeePickerViewTag;
+    
+    experiencePickerView = [[UIPickerView alloc]init];
+    _txtExperienceFrom.inputView = experiencePickerView;
+    _txtExperienceTo.inputView = experiencePickerView;
+    experiencePickerView.delegate = self;
+    experiencePickerView.dataSource = self;
+    experiencePickerView.tag = ExperiencePickerViewTag;
     
     gesture.delegate=self;
     gesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(Tapping)];
@@ -118,7 +155,12 @@
     {
         return 1;
     }
-    
+    else if (pickerView.tag == AgePickerViewTag){
+        return 1;
+    }
+    else if (pickerView.tag == FeePickerViewTag){
+        return 1;
+    }
     return 1;
 }
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
@@ -132,7 +174,15 @@
     {
         return self.genderArray.count;
     }
-    
+    else if (pickerView.tag == AgePickerViewTag){
+        return self.ageArray.count;
+    }
+    else if (pickerView.tag == FeePickerViewTag){
+        return self.feeArray.count;
+    }
+    else if (pickerView.tag == ExperiencePickerViewTag){
+        return self.experienceArray.count;
+    }
     return 1;
 }
 -(NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
@@ -145,7 +195,18 @@
     {
         return [self.genderArray[row] valueForKey:@"label"];
     }
-   
+    else if (pickerView.tag == AgePickerViewTag)
+    {
+        return [self.ageArray[row] valueForKey:@"label"];
+    }
+    else if (pickerView.tag == FeePickerViewTag)
+    {
+        return [self.feeArray[row] valueForKey:@"label"];
+    }
+    else if (pickerView.tag == ExperiencePickerViewTag)
+    {
+        return [self.experienceArray[row] valueForKey:@"label"];
+    }
     return nil;
     
 }
@@ -162,10 +223,34 @@
         self.selectedGender = self.genderArray[row];
         _txtGender.text = [self.genderArray[row] valueForKey:@"label"];
         [_txtGender resignFirstResponder];
+    }
+    else if (pickerView.tag == AgePickerViewTag)
+    {
+        self.selectedFromAge = self.ageArray[row];
+        _txtAgeFrom.text = [self.ageArray[row] valueForKey:@"label"];
+        _txtAgeTo.text = [self.ageArray[row] valueForKey:@"label"];
+        [_txtAgeFrom resignFirstResponder];
+        [_txtAgeTo resignFirstResponder];
         NSLog(@"slectd Gender:%@",self.selectedType);
     }
-    
-    
+    else if (pickerView.tag == FeePickerViewTag)
+    {
+        self.selectedFromFee = self.feeArray[row];
+        _txtFeeFrom.text = [self.feeArray[row] valueForKey:@"label"];
+        _txtFeeTo.text = [self.feeArray[row] valueForKey:@"label"];
+        [_txtFeeFrom resignFirstResponder];
+        [_txtFeeTo resignFirstResponder];
+        NSLog(@"slectd Gender:%@",self.selectedType);
+    }
+    else if (pickerView.tag == ExperiencePickerViewTag)
+    {
+        self.selectedFromExperience = self.experienceArray[row];
+        _txtExperienceFrom.text = [self.experienceArray[row] valueForKey:@"label"];
+        _txtExperienceTo.text = [self.experienceArray[row] valueForKey:@"label"];
+        [_txtExperienceFrom resignFirstResponder];
+        [_txtExperienceTo resignFirstResponder];
+        NSLog(@"slectd Gender:%@",self.selectedType);
+    }
 }
 -(void)Tapping
 {
@@ -203,7 +288,9 @@
         self.filterCriteriaData = [responseObject valueForKey:Datakey];
         [self gettingTypeArrayFromresponse];
         [self gettingGenderFromResponse];
-        
+        [self gettingAgeFromResponse];
+        [self gettingFeeFromResponse];
+        [self gettingExperienceFromResponse];
     } FailureBlock:^(NSString *errorDescription, id errorResponse) {
         [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
         NSString *errorMessage;
@@ -235,7 +322,33 @@
 }
 
 -(void)gettingAgeFromResponse{
-    
+    id indicesDetails = [self.filterCriteriaData valueForKey:@"indices"];
+    int index = [[indicesDetails valueForKey:@"age"] intValue];
+    self.ageArray = [[[self.filterCriteriaData valueForKey:@"filter_data"] objectAtIndex:index] valueForKey:@"values"];
+    self.selectedFromAge = [self.ageArray objectAtIndex:0];
+    self.selectedToAge = [self.ageArray objectAtIndex:0];
+    self.txtAgeFrom.text = [self.selectedFromAge valueForKey:@"label"];
+    self.txtAgeTo.text = [self.selectedToAge valueForKey:@"label"];
+}
+
+-(void)gettingFeeFromResponse{
+    id indicesDetails = [self.filterCriteriaData valueForKey:@"indices"];
+    int index = [[indicesDetails valueForKey:@"fee"] intValue];
+    self.feeArray = [[[self.filterCriteriaData valueForKey:@"filter_data"] objectAtIndex:index] valueForKey:@"values"];
+    self.selectedFromFee = [self.feeArray objectAtIndex:0];
+    self.selectedTofee = [self.feeArray objectAtIndex:0];
+    self.txtFeeFrom.text = [self.selectedFromFee valueForKey:@"label"];
+    self.txtFeeTo.text = [self.selectedTofee valueForKey:@"label"];
+}
+
+-(void)gettingExperienceFromResponse{
+    id indicesDetails = [self.filterCriteriaData valueForKey:@"indices"];
+    int index = [[indicesDetails valueForKey:@"experience"] intValue];
+    self.experienceArray = [[[self.filterCriteriaData valueForKey:@"filter_data"] objectAtIndex:index] valueForKey:@"values"];
+    self.selectedFromExperience = [self.experienceArray objectAtIndex:0];
+    self.selectedToExperience = [self.experienceArray objectAtIndex:0];
+    self.txtExperienceFrom.text = [self.selectedFromExperience valueForKey:@"label"];
+    self.txtExperienceTo.text = [self.selectedToExperience valueForKey:@"label"];
 }
 
 - (IBAction)maleButtonAction:(UIButton *)sender {
